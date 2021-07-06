6d5e9b3cb9d648561b68e0baea6518a77855277d
satyam-seth <satyam1998.1998@gmail.com>
2021-07-06 20:10:03 +0530

    design home page

--- url_shortener/core/form.py
+++ url_shortener/core/form.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class UrlForm(forms.Form):
-    url = forms.CharField(label="URL")
+    url = forms.CharField(label="URL",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter your url'}))
